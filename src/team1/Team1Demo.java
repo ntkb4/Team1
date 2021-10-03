1caf5676cd0c7ba5b956bf1a53be4663300bc898
jamilabensalem <jamilabensalem@localhost>
2021-10-03 12:51:11 -0400

    hi

--- src/team1/Team1Demo.java
+++ src/team1/Team1Demo.java
@@ -5,12 +5,13 @@ public class Team1Demo {
 	public static void main(String[] args) {
 		System.out.println("TEam2Demo");
 		System.out.println("trap...");
-		System.out.println("More Trap...");
 	}
 	
 	public void addNum(int a, int b) {
 		
 		System.out.println(a+b);
+		System.out.println("me jamila");
 		
 	}
+
 }

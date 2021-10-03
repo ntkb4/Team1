bb0419a7bf3982ca1ff359fca6a6bdc5558a206f
ntkb4 <91574282+ntkb4@users.noreply.github.com>
2021-10-03 12:41:04 -0400

    Update Team1Demo.java
    :)

--- src/team1/Team1Demo.java
+++ src/team1/Team1Demo.java
@@ -5,6 +5,7 @@ public class Team1Demo {
 	public static void main(String[] args) {
 		System.out.println("TEam2Demo");
 		System.out.println("trap...");
+		System.out.println("More Trap...");
 	}
 	
 	public void addNum(int a, int b) {
